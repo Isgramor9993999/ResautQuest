c9741677125b1c1d45764faa977d265501a32395
Isgramor <maksaj9@gmail.com>
2020-05-14 03:42:09 +0400

    Первые изменения

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import JoinBlock from './components/JoinBlock.jsx'
 
+
 function App() {
   return (
   <div className="wrapper">
--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import socket from '../socket';
+
 
 function JoinBlock() {
     return (
     <div className='join-block'>
-    <input type="text" placeholder="Room ID" value="" />
-    <input type="text" placeholder="User" value="" />
-    <button class="btn btn-success">Войти!</button>
+    <input type="text" placeholder="Room ID" />
+    <input type="text" placeholder="User" />
+    <button className="btn btn-success">Войти!</button>
   </div>
    );
 }
